5226692584c3be04b214e4ac39c39b9958113354
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-01-06 09:35:11 +0300

    Update views.py

--- properties/views.py
+++ properties/views.py
@@ -5,6 +5,7 @@ from .utils import get_all_properties
 @cache_page(60 * 15)
 def property_list(request):
     properties = get_all_properties()
+
     data = [
         {
             "id": p.id,
@@ -12,7 +13,9 @@ def property_list(request):
             "description": p.description,
             "price": str(p.price),
             "location": p.location,
+            "created_at": p.created_at,
         }
         for p in properties
     ]
-    return JsonResponse(data, safe=False)
+
+    return JsonResponse({"properties": data})
