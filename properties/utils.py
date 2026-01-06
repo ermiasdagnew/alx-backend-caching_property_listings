e25c4275f143001cfe829740f884b925d2eb2472
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-01-06 09:37:06 +0300

    Refactor cache metrics and remove unused function
    Removed unused caching function and improved hit ratio calculation.

--- properties/utils.py
+++ properties/utils.py
@@ -1,28 +1,18 @@
-from django.core.cache import cache
-from django_redis import get_redis_connection
-from .models import Property
-import logging
-
-logger = logging.getLogger(__name__)
-
-def get_all_properties():
-    properties = cache.get('all_properties')
-    if properties is None:
-        properties = list(Property.objects.all())
-        cache.set('all_properties', properties, 3600)
-    return properties
-
 def get_redis_cache_metrics():
     redis_conn = get_redis_connection("default")
     info = redis_conn.info()
+
     hits = info.get("keyspace_hits", 0)
     misses = info.get("keyspace_misses", 0)
-    total = hits + misses
-    hit_ratio = hits / total if total > 0 else 0
+
+    total_requests = hits + misses
+    hit_ratio = hits / total_requests if total_requests > 0 else 0
+
     metrics = {
         "keyspace_hits": hits,
         "keyspace_misses": misses,
         "hit_ratio": hit_ratio,
     }
+
     logger.info(metrics)
     return metrics
