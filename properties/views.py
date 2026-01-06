7e0b3fa4a7569d5d2fabb0587d9fd05518cc2dcd
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-01-06 09:32:59 +0300

    Add files via upload

---
+++ properties/views.py
@@ -0,0 +1,18 @@
+from django.views.decorators.cache import cache_page
+from django.http import JsonResponse
+from .utils import get_all_properties
+
+@cache_page(60 * 15)
+def property_list(request):
+    properties = get_all_properties()
+    data = [
+        {
+            "id": p.id,
+            "title": p.title,
+            "description": p.description,
+            "price": str(p.price),
+            "location": p.location,
+        }
+        for p in properties
+    ]
+    return JsonResponse(data, safe=False)
